1b3b544c8a892faf8c90a9ce2c872db2416098eb
Elliot <esneeringer@pubsvs.com>
2018-05-31 08:25:00 -0400

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SendWelcomeEmail;
+use Illuminate\Http\Request;
+use Log;
+use Mail;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        return view()->make("home.index");
+    }
+
+    public function send()
+    {
+        Log::info("Request Cycle with Queues Begins");
+        $this->dispatch((new SendWelcomeEmail())->delay(60 * 1));
+        Log::info("Request Cycle with Queues Ends");
+    }
+}
